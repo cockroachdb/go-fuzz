a54b91b28f49aa032fe40fad8f53e362153c1d98
Matt Jibson <matt.jibson@gmail.com>
2016-10-07 01:35:25 -0400

    Update to current cockroach
    Use pgwire corpus from our tests and switch to a multi-message use.

--- examples/pgwire/main.go
+++ examples/pgwire/main.go
@@ -3,12 +3,16 @@
 package pgwire
 
 import (
-	"encoding/binary"
+	"bytes"
+	"io"
+	"io/ioutil"
 	"net"
 	"strings"
 	"time"
 
+	"github.com/cockroachdb/cockroach/base"
 	"github.com/cockroachdb/cockroach/server"
+	"github.com/cockroachdb/cockroach/testutils/serverutils"
 )
 
 var expected = []string{
@@ -43,50 +47,22 @@ func Fuzz(data []byte) int {
 		}
 	}()
 
-	// Run in insecure mode to avoid dealing with TLS.
-	ctx := server.MakeTestContext()
-	ctx.Insecure = true
-
-	s := server.StartTestServerWithContext(nil, &ctx)
-	defer s.Stop()
+	serverutils.InitTestServerFactory(server.TestServerFactory)
+	s, err := serverutils.StartServerRaw(base.TestServerArgs{
+		Insecure: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer s.Stopper().Stop()
 
 	conn, err := net.Dial("tcp", s.ServingAddr())
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-
-	// Connect to the PGWire v3 server. This connection message includes information
-	// to match with pgwire.Match and bypass sql/pgwire.parseOptions:
-	// - 4 byte prefix for the buffer's length
-	// - 4 bytes for the version30 value
-	// - 1 byte for a null terminator to skip session args
-	const version30 = 196608
-	var connectBuf [9]byte
-	binary.BigEndian.PutUint32(connectBuf[:4], uint32(len(connectBuf)))
-	binary.BigEndian.PutUint32(connectBuf[4:8], version30)
-	if err != nil {
-		panic(err)
-	}
-	conn.Write(connectBuf[:])
-	time.Sleep(1 * time.Millisecond)
-
-	// Send a pgwire "typed message" (see sql/pgwire.readBuffer.{readTypedMsg, readUntypedMsg}).
-	// The message includes:
-	// - 1 byte for the type
-	// - 4 bytes for the message's length (minus the type prefix)
-	// - the rest of message
-	//
-	// TODO(nvanbenschoten) investigate sending multiple messages.
-	sendBuf := data
-	if len(data) > 1 {
-		newLen := len(data) + 4
-		sendBuf = make([]byte, newLen)
-		sendBuf[0] = data[0]
-		binary.BigEndian.PutUint32(sendBuf[1:5], uint32(newLen-1))
-		copy(sendBuf[5:], data[1:])
-	}
-	conn.Write(sendBuf)
+	go io.Copy(ioutil.Discard, conn)
+	io.Copy(conn, bytes.NewReader(data))
 	time.Sleep(1 * time.Millisecond)
 	return 0
 }
